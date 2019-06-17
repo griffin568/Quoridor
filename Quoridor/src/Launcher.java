87b0720fd9ca57ebd8dd79b5659ab020556f9de2
Drmarsupial35 <you@example.com@gmail.com>
2019-06-17 11:29:00 +0200

    MAJ Partie, Launcher

--- Quoridor/src/Launcher.java
+++ Quoridor/src/Launcher.java
@@ -1,3 +1,6 @@
+import quoridor.*;
+import java.util.ArrayList;
+
 /**
   * Cette classe permet de lancer l'application
   * @author AlexM02 , Drmarsupial35 , Eclixal , griffin568
@@ -5,6 +8,8 @@
 public class Launcher {
 
     public static void main(String[] args) {
-
+      Partie test = new Partie(Mode.HH);
+      ArrayList<Pion> lesPions = new ArrayList<Pion>();
+      System.out.println(test.getPlateau().toString(lesPions));
     }
 }
--- Quoridor/src/quoridor/Partie.java
+++ Quoridor/src/quoridor/Partie.java
@@ -13,25 +13,21 @@ public class Partie {
     private int tour;
     private Mode mode;
     private Plateau plateau;
-    private ArrayList<Joueur> joueur;
+    private ArrayList<Joueur> joueurs;
     private ArrayList<Barriere> barrieres;
 
     /**
       * Créé un nouvel objet Partie
       * @param fileName le nom du fichier de configuration
       */
-    public Partie(String fileName) {
+    public Partie(Mode mode) {
       try {
-        if (fileName == null) {
-          throw new Exception("Partie constructeur - Le nom du fichier doit être valide pour pouvoir être utilisé.");
+        if (mode == null) {
+          throw new Exception("Partie constructeur - Le mode de jeu doit être valide pour pouvoir être utilisé.");
         }
         else {
-          barrieres = new ArrayList<Barriere>();
-          initialisation();
+          initialisation(mode);
         }
-      }
-      catch(FileNotFoundException e) {
-        System.err.println("Partie construceur - Fichier non trouvé (" + fileName + ")");
       }
       catch(Exception e) {
         System.err.println(e.getMessage());
@@ -43,7 +39,22 @@ public class Partie {
     /**
       * Sauvegarde la partie
       */
-    public void sauvegarder() {
+    public void sauvegarder(String fileName, Joueur leJoueur) {
+      try {
+        if (fileName == null) {
+          throw new Exception("Partie sauvegarder() - Le nom du fichier doit exister");
+        }
+        else if (leJoueur == null) {
+          throw new Exception("Partie sauvegarder() - Le dernier joueur ayant joué doit exister");
+        }
+        else {
+          RWFile.writeFile(fileName, this.joueurs, this.barrieres, this.tour, leJoueur);
+          System.out.println("La partie a bien été sauvegardé !");
+        }
+      }
+      catch (Exception e) {
+        System.err.println(e.getMessage());
+      }
 
     }
 
@@ -72,30 +83,34 @@ public class Partie {
     /**
       * Initialise les différents éléments constants de la partie
       */
-    private void initialisation() {
-      ArrayList<String> lignes = RWFile.readFile("config");
+    private void initialisation(Mode mode) {
+          this.barrieres = new ArrayList<Barriere>();
+          this.tour = 0;
+          this.mode = mode;
+          configuration(mode);
     }
 
     /**
       * Configure les éléments non constants de la partie à l'aide du fichier de configuration
       * @param filename le nom du fichier de configuration
       */
-    private void configuration(String filename) {
-      try {
-        if (filename == null) {
-          throw new Exception("Partie constructeur - Le nom du fichier doit être valide pour pouvoir être utilisé.");
-        }
-        else {
+    private void configuration(Mode mode) {
+
+      Barriere b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,b18,b19,b20;
+
+        if (mode.equals(Mode.HH) || mode.equals(Mode.HI)) {
+          ArrayList<String> lignes = RWFile.readFile("config2");
+          this.plateau = new Plateau(Integer.parseInt(lignes.get(0)));
+
+
 
         }
-      }
-      catch(FileNotFoundException e) {
-        System.err.println("Partie construceur - Fichier non trouvé (" + filename + ")");
-      }
-      catch(Exception e) {
-        System.err.println(e.getMessage());
-      }
-    }
+        else {
+          ArrayList<String> lignes = RWFile.readFile("config4");
+          this.plateau = new Plateau(Integer.parseInt(lignes.get(0)));
+        }
+
+        }
 
     /**
       * Lance la partie
@@ -135,7 +150,7 @@ public class Partie {
   * @return le numéro du tour
   */
   public int getTour() {
-    return tour;
+    return this.tour;
   }
 
   /**
@@ -143,6 +158,10 @@ public class Partie {
   * @return le mode de jeu utilisé
   */
   public Mode getMode() {
-    return mode;
+    return this.mode;
+  }
+
+  public Plateau getPlateau() {
+    return this.plateau;
   }
 }
