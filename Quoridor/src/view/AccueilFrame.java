983ed0ff41da039878880574a57f965f8840cbc6
Drmarsupial35 <drmarsupial35@gmail.com>
2019-06-19 15:20:51 +0200

    JavaDoc AccueilFrame

--- Quoridor/src/view/AccueilFrame.java
+++ Quoridor/src/view/AccueilFrame.java
@@ -4,18 +4,29 @@ import javax.swing.*;
 import java.awt.*;
 import view.controller.*;
 
+/**
+  * Cette classe gère la création de la page d'acccueil du jeu et de ses composants graphiques.
+  * @author Drmarsupial35, griffin568
+  * @version 0.1.0
+  */
 public class AccueilFrame extends JPanel {
 
   private MainFrame parent;
   private JButton PhotoButton, LancerBton, ChargerBton, QuitterBton;
   private JPanel chargement, choixNombre;
+  private JLabel titre;
 
+  /**
+    * Créer une page d'acccueil vide. Ce constructeur est utilisé pour permettre son utilisation future dans d'autres classes.
+    */
+  public AccueilFrame() {}
 
-  public AccueilFrame() {
-    
-  }
-
-
+  /**
+    * Créer la page d'acccueil de l'application. Cette méthode appele la méthode privée initComponent()
+    * @param parent La fenetre principale de jeu qui est utilisée pour changer l'écran affiché
+    * @param chargement L'écran permettant de choisir la partie que l'on souhaite charger
+    * @param choixNombre L'écran permettant de créer une nouvelle partie et de choisir le nombre de joueurs de cette dernière
+    */
   public AccueilFrame(MainFrame parent, JPanel chargement, JPanel choixNombre) {
     try {
       if (parent == null) {
@@ -40,6 +51,9 @@ public class AccueilFrame extends JPanel {
     }
   }
 
+  /**
+    *Initialise les composants graphiques de la page d'acccueil, tel que les boutons cliquables ou encore l'image du jeu.
+    */
   private void initComponent() {
     this.setLayout(new BorderLayout());
     JPanel leftContainer = new JPanel(new BorderLayout());
