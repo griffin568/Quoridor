fa20b71329e319206d11dbe50212ebbf009f315d
Titouan Loisel <drmarsupial35@gmail.com>
2019-06-19 11:20:07 +0200

    Création DownButtonListener

---
+++ Quoridor/src/view/controller/DownButtonListener.java
@@ -0,0 +1,43 @@
+package view.controller;
+
+import javax.swing.event.*;
+import java.awt.event.*;
+import javax.swing.*;
+import view.*;
+
+public class DownButtonListener implements ActionListener {
+
+  private MainFrame mainF;
+
+  /**
+    *
+    * @param mainF
+    */
+  public DownButtonListener(MainFrame mainF) {
+    try {
+      if (mainF == null) {
+        throw new Exception("L'écran principal doit exister pour être utiliser dans le listener.");
+      }
+      else {
+        this.mainF = mainF;
+      }
+    }
+    catch(Exception e) {
+      System.err.println(e.getMessage());
+    }
+  }
+
+  /**
+    *
+    * @param mainF
+    */
+  public void actionPerformed(ActionEvent ev) {
+    JButton source = (JButton)ev.getSource()
+    if (source.getText().equalsIgnoreCase("QUITTER")) {
+      System.exit(0);
+    }
+    else if (source.getText().equalsIgnoreCase("RETOUR")) {
+      this.mainF.getSwitchableCL().show()
+    }
+  }
+}
